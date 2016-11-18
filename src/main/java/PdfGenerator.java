8233d4c11bbb0d168ab861ad8f26da017051f0b3
maciekdeb <maciekdeb@gmail.com>
2016-11-18 16:37:24 +0100

    asd

--- src/main/java/PdfGenerator.java
+++ src/main/java/PdfGenerator.java
@@ -1,44 +1,72 @@
-import com.lowagie.text.*;
-import com.lowagie.text.pdf.BaseFont;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
-import org.jsoup.Jsoup;
+//
 
-import java.io.FileOutputStream;
-import java.io.IOException;
+//import com.lowagie.text.*;
+//import com.lowagie.text.pdf.BaseFont;
+//import com.lowagie.text.pdf.PdfPCell;
+//import com.lowagie.text.pdf.PdfPTable;
+//import com.lowagie.text.pdf.PdfWriter;
+//import org.jsoup.Jsoup;
+//
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.util.Arrays;
 
 /**
  * Created by maciej.debowski on 17.11.2016.
  */
 public class PdfGenerator {
 
-    public static void main(String[] args) throws DocumentException, IOException {
-        String fontPath = "C:\\Users\\maciej.debowski\\Downloads\\arialbd.ttf";
-        BaseFont baseFont = BaseFont.createFont(fontPath, BaseFont.IDENTITY_H, true);
-        String arabic = "Maciek \u0627\u0644\u0647\u0627\u062a\u0641           |";
-        String arabic2 = "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+&#1641;&#1638;&#1637;&#1634;&#1637;&#1635;&#1632;&#1641;&#1641;&#1641;&#1639;&nbsp;:&#1575;&#1604;&#1607;&#1575;&#1578;&#1601;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>";
-        arabic2 = Jsoup.parse(arabic2).text();
-        Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
-        document.open();
+    public static void main(String[] args) {//throws DocumentException, IOException {
+//        String fontPath = "C:\\Users\\maciej.debowski\\Downloads\\arialbd.ttf";
+//        BaseFont baseFont = BaseFont.createFont(fontPath, BaseFont.IDENTITY_H, true);
+//
+//        java.util.List<String> lines = Arrays.asList(
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kuwait&nbsp;-&nbsp;360&nbsp;Mall&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+&#1641;&#1638;&#1637;&#1634;&#1637;&#1635;&#1632;&#1641;&#1641;&#1641;&#1639;&nbsp;:&#1575;&#1604;&#1607;&#1575;&#1578;&#1601;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#1633;&#1635;:&#1633;&#1632;:&#1633;&#1635;&nbsp;&nbsp;&#1633;&#1641;/&#1633;&#1632;/&#1633;&#1638;&nbsp;:&#1578;&#1575;&#1585;&#1610;&#1582;&nbsp;&nbsp;&nbsp;&#1634;&#1633;&#1640;&#1641;&#1639;&nbsp;:&#1578;&#1584;&#1603;&#1585;&#1577;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#1634;&#1640;&#1639;&#1632;&#1633;&nbsp;:&#1605;&#1578;&#1580;&#1585;&nbsp;&nbsp;&nbsp;&#1636;&nbsp;:&#1578;&#1587;&#1580;&#1610;&#1604;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;990001&nbsp;&nbsp;:&#1571;&#1605;&#1610;&#1606;&nbsp;&#1575;&#1604;&#1589;&#1606;&#1583;&#1608;&#1602;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;JENNIFER&nbsp;MIKE&nbsp;:&#1575;&#1604;&#1593;&#1605;&#1610;&#1604;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2300263323&nbsp;&#1607;&#1608;&#1610;&#1577;&nbsp;&#1575;&#1604;&#1593;&#1605;&#1610;&#1604;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>",
+//                "<span class=\"font_0\"><b><u>&nbsp;&nbsp;&nbsp;&nbsp;&#1575;&#1604;&#1605;&#1576;&#1604;&#1594;&#1587;&#1593;&#1585;&nbsp;&#1575;&#1604;&#1608;&#1581;&#1583;&#1577;&nbsp;&nbsp;&nbsp;&nbsp;&#1575;&#1604;&#1603;&#1605;&#1610;&#1577;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#1602;&#1591;&#1593;&#1577;&nbsp;</u></b></span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;806727274&nbsp;&nbsp;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NOSIZ&nbsp;-&nbsp;9022&nbsp;-&nbsp;00018GA7M0Z&nbsp;&nbsp;&nbsp;HANDBAGS</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&#1640;&#1640;&#1632;.&#1632;&#1632;&nbsp;&nbsp;&nbsp;&nbsp;&#1640;&#1640;&#1632;.&#1632;&#1632;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#1633;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>",
+//                "<span class=\"font_0\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;990001&nbsp;(MANAGER)&nbsp;&nbsp;:&#1605;&#1606;&#1583;&#1608;&#1576;&nbsp;&#1576;&#1610;&#1593;&nbsp;</span><br/>"
+//        );
+//        StringBuilder linesToCell = new StringBuilder();
+//        for (String line : lines) {
+//            linesToCell.append(Jsoup.parse(line).text());
+//        }
+//
+//        Document document = new Document();
+//        PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+//        document.open();
+//
+//        PdfPTable table = new PdfPTable(1);
+//        table.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
+//
+//        Image image = Image.getInstance("C:\\Users\\maciej.debowski\\AppData\\Local\\Temp\\DtvResource6580447374521984865.bmp");
+//        Image image2 = Image.getInstance("C:\\Users\\maciej.debowski\\AppData\\Local\\Temp\\DtvResource5330641316123788731.bmp");
+//        table.addCell(new PdfPCell(image2));
+//        table.addCell(new PdfPCell(image));
+//
+//        for(String line : lines){
+//            PdfPCell cell = new PdfPCell(new Paragraph(StringEscapeUtils.unescapeHtml4(line.replaceAll("\\<.*?>","")), new Font(baseFont, 16)));
+//            PdfPCell cell = new PdfPCell(new Paragraph( Jsoup.parse(line).text(), new Font(baseFont, 16)));
 
-        PdfPTable table = new PdfPTable(1);
-        table.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
-
-        PdfPCell cell = new PdfPCell(new Paragraph(arabic+arabic2, new Font(baseFont, 20)));
-        cell.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
-        cell.setBorder(Rectangle.NO_BORDER);
-        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        PdfPCell cell2 = new PdfPCell(new Paragraph(arabic2, new Font(baseFont, 20)));
-        cell2.setBorder(Rectangle.NO_BORDER);
-
-        table.addCell(cell);
-//        table.addCell(cell2);
-
-        document.add(table);
-
-        document.close();
+//            cell.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
+//            cell.setBorder(Rectangle.NO_BORDER);
+//            cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+//            table.addCell(cell);
+//        }
+//
+//        document.add(table);
+//        document.close();
     }
 
 }
--- src/main/java/PdfGenerator2.java
+++ src/main/java/PdfGenerator2.java
@@ -19,89 +19,86 @@
 //import java.io.IOException;
 //import java.util.ArrayList;
 //
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+import com.itextpdf.tool.xml.ElementList;
+import com.itextpdf.tool.xml.XMLWorker;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.tool.xml.html.CssAppliers;
+import com.itextpdf.tool.xml.html.CssAppliersImpl;
+import com.itextpdf.tool.xml.html.Tags;
+import com.itextpdf.tool.xml.parser.XMLParser;
+import com.itextpdf.tool.xml.pipeline.css.CSSResolver;
+import com.itextpdf.tool.xml.pipeline.css.CssResolverPipeline;
+import com.itextpdf.tool.xml.pipeline.end.ElementHandlerPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 /**
  * Created by maciej.debowski on 17.11.2016.
  */
 public class PdfGenerator2 {
 
-    public static void main(String[] args){// throws DocumentException, IOException {
+    public static void main(String[] args) throws IOException, DocumentException {// throws DocumentException, IOException {
+        Document document = new Document(PageSize.LEGAL);
+        String fontPath = "c:/windows/fonts/cour.ttf";
+        BaseFont baseFont = BaseFont.createFont(fontPath, BaseFont.IDENTITY_H, true);
+        XMLWorkerFontProvider fontProvider = new XMLWorkerFontProvider(XMLWorkerFontProvider.DONTLOOKFORFONTS);
+        fontProvider.register( "C:\\Users\\maciej.debowski\\Downloads\\arialbd.ttf", BaseFont.IDENTITY_H);
+        CssAppliers cssAppliers = new CssAppliersImpl(fontProvider);
+        HtmlPipelineContext htmlContext = new HtmlPipelineContext(cssAppliers);
+        htmlContext.setTagFactory(Tags.getHtmlTagProcessorFactory());
 
-//        Document document = new Document(PageSize.LEGAL);
-//
-//        fonts
-//        XMLWorkerFontProvider fontProvider = new XMLWorkerFontProvider(XMLWorkerFontProvider.DONTLOOKFORFONTS);
-//        fontProvider.register( "C:\\Users\\maciej.debowski\\Downloads\\arialbd.ttf", BaseFont.IDENTITY_H);
-//        fontProvider.register("/Users/ibrahimbakhsh/Library/Fonts/trado.otf", BaseFont.IDENTITY_H);
-//        fontProvider.register("/Users/ibrahimbakhsh/Library/Fonts/tahoma.ttf", BaseFont.IDENTITY_H);
-//        CssAppliers cssAppliers = new CssAppliersImpl(fontProvider);
-//        HtmlPipelineContext htmlContext = new HtmlPipelineContext(cssAppliers);
-//        htmlContext.setTagFactory(Tags.getHtmlTagProcessorFactory());
-//
-//         CSS
-//        CSSResolver cssResolver =
-//                XMLWorkerHelper.getInstance().getDefaultCssResolver(true);
-//
-//
-//
-//         Pipelines
-//        ElementList elements = new ElementList();
-//        ElementHandlerPipeline end = new ElementHandlerPipeline(elements, null);
-//        HtmlPipeline html = new HtmlPipeline(htmlContext, end);
-//        CssResolverPipeline css = new CssResolverPipeline(cssResolver, html);
-//
-//         HTML
-//        htmlContext.setTagFactory(Tags.getHtmlTagProcessorFactory());
-//        htmlContext.autoBookmark(false);
-//
-//
-//         XML Worker
-//        XMLWorker worker = new XMLWorker(css, true);
-//        XMLParser p = new XMLParser(worker);
-//        p.parse(new FileInputStream("C:\\Users\\maciej.debowski\\Documents\\arabic.html"));
-//
-//
-//        writer
-//        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("output2.pdf"));
-//        writer.setInitialLeading(12.5f);
-//        writer.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
-//
-//
-//         step 4
-//        document.open();
-//
-//
-//         step 5
-//
+        CSSResolver cssResolver =
+                XMLWorkerHelper.getInstance().getDefaultCssResolver(true);
+
+        ElementList elements = new ElementList();
+        ElementHandlerPipeline end = new ElementHandlerPipeline(elements, null);
+        HtmlPipeline html = new HtmlPipeline(htmlContext, end);
+        CssResolverPipeline css = new CssResolverPipeline(cssResolver, html);
+
+        htmlContext.setTagFactory(Tags.getHtmlTagProcessorFactory());
+        htmlContext.autoBookmark(false);
+
+
+        XMLWorker worker = new XMLWorker(css, true);
+        XMLParser p = new XMLParser(worker);
+        p.parse(new FileInputStream("C:\\Users\\maciej.debowski\\Documents\\arabic.html"), Charset.forName("UTF-8"));
+
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("output2.pdf"));
+        writer.setInitialLeading(12.5f);
+
+        document.open();
+
+        PdfPTable table = new PdfPTable(1);
+        table.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
+
+        String a0 = "+";
+        String a1 = "٩٦٥٢٥٣٠٩٩٩٧";
+        String a2 = " :";
+        String a3 = "الهاتف";
+
+        table.addCell(new Phrase("الهاتف: ٩٦٥٢٥٣٠٩٩٩٧+", new Font(baseFont)));
+        table.addCell(new Phrase(a0 + a1 + a2 + a3, new Font(baseFont)));
+        table.addCell(new Phrase("أمين الصندوق: 990001", new Font(baseFont)));
+        table.addCell(new Phrase(" ", new Font(baseFont)));
+
 //        for (Element e : elements) {
-//            out.println(e.toString());
-//            if(e instanceof PdfPTable){
-//                PdfPTable t = (PdfPTable) e;
-//                t.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
-//                ArrayList<PdfPRow> rows = t.getRows();
-//                for(PdfPRow row:rows){
-//                    PdfPCell[] cells = row.getCells();
-//                    for(PdfPCell cell:cells){
-//                        cell.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
-//                    }
-//                }
-//                e = t;
-//            }
-//            document.add(e);
+//            PdfPCell cell = new PdfPCell();
+//            e.getChunks().get(0).setFont(new Font(baseFont));
+//            cell.addElement(e);
+//            table.addCell(cell);
 //        }
-//
-//
-//        try adding new table
-//
-//        PdfPTable table = new PdfPTable(1);
-//        table.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
-//        Font f = new Font(BaseFont.createFont("C:\\Users\\maciej.debowski\\Downloads\\arialbd.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED));
-//        PdfPCell cell = new PdfPCell(new Paragraph("تجربة نص عربي",f));
-//        table.addCell(cell);
-//        document.add(table);
-//
-//         step 6
-//        document.close();
+        document.add(table);
 
+        document.close();
     }
 
 }
